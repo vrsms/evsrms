9ab0d236f831cb8ee7c13a04259cdb81d6920998
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-24 19:53:47 +0300

    Add models

--- service/models.py
+++ service/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Vehicle(models.Model):
+    make = models.CharField(max_length=30)
+    model = models.CharField('vehicle_model', max_length=20)
+
+
+class Service(models.Model):
+    service_ticket = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    service_date = models.DateTimeField('Service Date')
+    cost = models.IntegerField(default=0)
