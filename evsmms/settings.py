90c2cc6b5c259bcc5ca15424b20952a364cc9288
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-24 16:50:45 +0300

    Add database connection

--- evsmms/settings.py
+++ evsmms/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-iyopy60$6m-4nsj*j9uhx!^28*hw@c_-x6zvg!y)3a@o39@fg1
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'evsmms.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'evsmms',
+        'USER': 'root',
+        'PASSWORD': '0101',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -99,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -112,7 +113,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
