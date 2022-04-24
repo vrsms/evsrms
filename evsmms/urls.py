61a6be14799280fc033402cdfcfd949f1aaa6c36
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-24 16:01:25 +0300

    Add service index view

--- evsmms/urls.py
+++ evsmms/urls.py
@@ -10,12 +10,13 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('service/', include('service.urls')),
     path('admin/', admin.site.urls),
 ]
