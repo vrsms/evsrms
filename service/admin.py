6f0e01dcb65cfc7d689f2dd025a9d66523e1bd01
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-24 20:21:16 +0300

    Register Service app to admin index page

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Vehicle
+from .models import Service
 
 # Register your models here.
+admin.site.register(Vehicle)
+admin.site.register(Service)
