90e65d388746fbdeb2d90fb1112c391ec3b89fb1
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-25 13:58:44 +0300

    Add detail view

--- service/urls.py
+++ service/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /service/5/
+    path('<int:vehicle_id>/', views.detail, name='detail'),
 ]
--- service/views.py
+++ service/views.py
@@ -1,11 +1,13 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-
+from django.http import Http404
+from django.http import HttpResponse
 from .models import Vehicle
 
 
 # Create your views here.
 
-def index(request):
+def index(request: WSGIRequest) -> HttpResponse | Http404:
     """
     Homepage view
     Returns the homepage of the service app
@@ -16,3 +18,14 @@ def index(request):
         'latest_vehicle_list': latest_vehicle_list,
     }
     return render(request, 'service/index.html', context)
+
+
+def detail(request: WSGIRequest, vehicle_id: int) -> HttpResponse | Http404:
+    """
+    Vehicle details
+    """
+    try:
+        vehicle = Vehicle.objects.get(pk=vehicle_id)
+    except Vehicle.DoesNotExist:
+        raise Http404("Vehicle does not exist")
+    return render(request, 'service/detail.html', {'vehicle': vehicle})
