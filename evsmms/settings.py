006bba852fc1cd4221f94a885ef439c9f4cbfa00
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-26 14:58:54 +0300

    Update system homepage

--- evsmms/settings.py
+++ evsmms/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'kyc.apps.KycConfig',
     'service.apps.ServiceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- kyc/views.py
+++ kyc/views.py
@@ -1,13 +1,20 @@
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 
 # Create your views here.
+from service.models import Vehicle
 
-def index(request):
+
+def index(request: WSGIRequest) -> HttpResponse | Http404:
     """
     Homepage view
-    Returns the homepage of the system
+    Returns the homepage of the service app
     """
+    latest_vehicle_list = Vehicle.objects.order_by('-make')[:5]
 
-    return HttpResponse("Hello, world! You're at the evsmms index.")
+    context = {
+        'latest_vehicle_list': latest_vehicle_list,
+    }
+    return render(request, 'kyc/index.html', context)
