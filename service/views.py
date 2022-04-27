5d89fa7ea0239ee4a6330f6d995c04d33a7ac1d5
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-27 18:25:50 +0300

    limit access only to logged in users

--- service/views.py
+++ service/views.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import Http404
 from django.http import HttpResponse
 from .models import Vehicle
@@ -12,6 +13,8 @@ def index(request: WSGIRequest) -> HttpResponse | Http404:
     Homepage view
     Returns the homepage of the service app
     """
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     latest_vehicle_list = Vehicle.objects.order_by('-make')[:5]
 
     context = {
@@ -24,5 +27,7 @@ def detail(request: WSGIRequest, vehicle_id: int) -> HttpResponse | Http404:
     """
     Vehicle details
     """
+    if not request.user.is_authenticated:
+        return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
     vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
     return render(request, 'service/detail.html', {'vehicle': vehicle})
