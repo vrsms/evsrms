078721c98bfc8025fed9e99400bab1a43e0e4318
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-27 17:19:59 +0300

    Optimize imports

--- kyc/views.py
+++ kyc/views.py
@@ -1,10 +1,9 @@
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 
 # Create your views here.
-from service.models import Vehicle
 
 
 def index(request: WSGIRequest) -> HttpResponse | Http404:
