a230544054551b06660f87e5681107c2e298bbe0
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-04-27 17:18:04 +0300

    Include native authentication framework

--- evsmms/urls.py
+++ evsmms/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('kyc.urls')),
     path('service/', include('service.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
