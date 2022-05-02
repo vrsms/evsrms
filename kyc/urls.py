96fe2e3728d82f34f8c90efc74842a938e89dca8
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-05-02 14:40:53 +0300

    Create password_reset_complete template

--- kyc/urls.py
+++ kyc/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -13,6 +14,8 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
