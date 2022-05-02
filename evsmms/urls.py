b4a0bb2e24673b5efd8b3a0f37b142acb4c049ee
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-05-02 09:05:20 +0300

    Include Django authentication URLConfig

--- evsmms/urls.py
+++ evsmms/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('', include('kyc.urls')),
     path('service/', include('service.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('kyc.urls')),
 ]
