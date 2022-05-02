17ebf8889c91c26e53154821726c4cb0a768ea75
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-05-02 09:28:43 +0300

    Include login logic

--- evsmms/settings.py
+++ evsmms/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login URL
+LOGIN_URL = 'accounts/login'
+
+# Login landing page
+LOGIN_REDIRECT_URL = '/'
