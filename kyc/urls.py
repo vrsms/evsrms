b961adc3c922760ac9b40667425ed1b0869c6862
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-05-03 09:33:25 +0300

    Namespace URLs

--- kyc/urls.py
+++ kyc/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'kyc'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
