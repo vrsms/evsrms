a4af39097c8df5e69ed3c91e1a71e3d87b595bc2
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-05-07 03:46:18 +0300

    Removed Vehicle model

--- service/models.py
+++ service/models.py
@@ -2,16 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-
-class Vehicle(models.Model):
-    make = models.CharField(max_length=30)
-    model = models.CharField('vehicle_model', max_length=30)
-    plate_number = models.CharField('Plate number', max_length=30)
-    category = models.CharField(max_length=30)
-    status = models.CharField('Operation status', max_length=30)
-
-    def __str__(self):
-        return self.plate_number
+from kyc.models import Vehicle
 
 
 class ServiceTicket(models.Model):
@@ -24,4 +15,3 @@ class ServiceTicket(models.Model):
 
     def __str__(self):
         return self.item_serviced
-
