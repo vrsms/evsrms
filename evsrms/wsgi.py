79662dd866de5fb7022ca554bc7b6d3050c0c7ea
Kaiza Ilomo <kaiza@kunonu.co.tz>
2022-05-25 06:51:55 +0300

    Rename evsmms app to evsrms

--- evsrms/wsgi.py
+++ evsrms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for evsmms project.
+WSGI config for evsrms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evsmms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evsrms.settings')
 
 application = get_wsgi_application()
